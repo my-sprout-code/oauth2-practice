fc7259b38d857e8a835168bc8451e1d86a8cba36
김우재 <kwj1270@naver.com>
2021-05-25 09:34:12 +0900

    fix(kwj1270):TestController conflict fix

--- src/main/java/ssafy/com/kkyuwoo/happyhouse/controller/TestController.java
+++ src/main/java/ssafy/com/kkyuwoo/happyhouse/controller/TestController.java
@@ -1,22 +1,13 @@
 package ssafy.com.kkyuwoo.happyhouse.controller;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-=======
-import org.springframework.ui.Model;
->>>>>>> d4cea819eeaa976cb42f932c31af11fd71d9c532
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
     @GetMapping("/")
-<<<<<<< HEAD
     public String index() {
-=======
-    public String start(Model model){
-        model.addAttribute("hello", "Hello World");
->>>>>>> d4cea819eeaa976cb42f932c31af11fd71d9c532
         return "index";
     }
 }
